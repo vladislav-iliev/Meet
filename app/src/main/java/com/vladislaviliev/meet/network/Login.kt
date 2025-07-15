cc2670df2231ab2dbb07a4e1b817d581c3e4c030
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-07-16 02:19:47 +0300

    Add Login

---
+++ app/src/main/java/com/vladislaviliev/meet/network/Login.kt
@@ -0,0 +1,16 @@
+package com.vladislaviliev.meet.network
+
+import okhttp3.OkHttpClient
+import org.openapitools.client.apis.CognitoControllerApi
+
+internal class Login {
+
+    private fun serverResponse(client: OkHttpClient, username: String, password: String) =
+        CognitoControllerApi(client = client).login(username, password)
+
+    fun login(client: OkHttpClient, username: String, password: String): Tokens {
+        val response = serverResponse(client, username, password)
+        val accessToken = response.accessToken
+        return Tokens(accessToken, response.refreshToken, TokenParser().parseExpiration(accessToken))
+    }
+}
