f8fd77072654e0e50bb5ea80106a1c05b0a2b679
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-07-16 17:13:59 +0300

    In Tokens, add BLANK functionality

--- app/src/main/java/com/vladislaviliev/meet/network/Tokens.kt
+++ app/src/main/java/com/vladislaviliev/meet/network/Tokens.kt
@@ -1,3 +1,9 @@
 package com.vladislaviliev.meet.network
 
-internal data class Tokens(val access: String, val refresh: String, val expiration: Long)
+internal data class Tokens(val access: String, val refresh: String, val expiration: Long) {
+    val isBlank = access.isNotBlank()
+
+    companion object {
+        val BLANK = Tokens("", "", -1)
+    }
+}
