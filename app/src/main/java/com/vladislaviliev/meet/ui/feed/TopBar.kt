6704f146cab6591acecfee6b6e443c1918b65c09
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-07-23 02:07:01 +0300

    Remove TopBar title bold font

--- app/src/main/java/com/vladislaviliev/meet/ui/feed/TopBar.kt
+++ app/src/main/java/com/vladislaviliev/meet/ui/feed/TopBar.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vladislaviliev.meet.R
@@ -33,16 +32,17 @@ import com.vladislaviliev.meet.ui.theme.MeetTheme
 internal fun FeedTopBar(modifier: Modifier = Modifier) {
     Surface(modifier.fillMaxWidth()) {
 
-        val horizontalPadding = dimensionResource(R.dimen.feed_list_padding) + dimensionResource(R.dimen.feed_card_padding)
+        val horizontalPadding =
+            dimensionResource(R.dimen.feed_list_padding) + dimensionResource(R.dimen.feed_card_padding)
         val verticalPadding = dimensionResource(R.dimen.feed_list_padding)
 
-        Column(Modifier.statusBarsPadding().padding(horizontalPadding, verticalPadding)) {
+        Column(
+            Modifier
+                .statusBarsPadding()
+                .padding(horizontalPadding, verticalPadding)
+        ) {
             Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.Bold
-                )
+                Text(stringResource(R.string.app_name), style = MaterialTheme.typography.headlineSmall)
                 Spacer(Modifier.weight(1f))
                 IconButton({}) { Icon(Icons.Filled.Tune, stringResource(R.string.filter)) }
                 IconButton({}) { Icon(Icons.Filled.Search, stringResource(R.string.search)) }
@@ -58,7 +58,8 @@ internal fun FeedTopBar(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true,
+@Preview(
+    showBackground = true, showSystemUi = true,
     uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
 )
 @Composable
