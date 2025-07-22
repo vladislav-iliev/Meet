30dbf244f1ec7763009956f8b1c40924a4c5dcfc
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-07-22 20:52:50 +0300

    Clean up UI in LoginScreen.kt

--- app/src/main/java/com/vladislaviliev/meet/ui/login/LoginScreen.kt
+++ app/src/main/java/com/vladislaviliev/meet/ui/login/LoginScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -87,13 +86,9 @@ private fun BoxScope.Contents(
             .align(Alignment.TopCenter),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            stringResource(R.string.sign_in_to_your_account),
-            style = MaterialTheme.typography.headlineSmall,
-            fontWeight = FontWeight.Bold
-        )
+        Text(stringResource(R.string.sign_in_to_your_account), style = MaterialTheme.typography.headlineSmall)
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(Modifier.height(16.dp))
 
         OutlinedTextField(
             value = email,
@@ -103,7 +98,7 @@ private fun BoxScope.Contents(
             singleLine = true,
         )
 
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(Modifier.height(4.dp))
 
         OutlinedTextField(
             value = password,
