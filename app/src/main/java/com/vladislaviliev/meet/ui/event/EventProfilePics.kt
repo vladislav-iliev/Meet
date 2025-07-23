d2864ba2d86abf5d1a064b9df09281c9ecfa2129
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-07-23 08:26:20 +0300

    Simplify `EventProfilePics` by inlining parameters and updating image styles

--- app/src/main/java/com/vladislaviliev/meet/ui/event/EventProfilePics.kt
+++ app/src/main/java/com/vladislaviliev/meet/ui/event/EventProfilePics.kt
@@ -5,39 +5,34 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.vladislaviliev.meet.R
 
 @Composable
-fun EventProfilePics(
-    uris: Iterable<String>,
-    modifier: Modifier = Modifier,
-    imageSize: Dp = 48.dp,
-    overlapPercentage: Float = 0.5f,
-    outlineWidth: Dp = 2.dp,
-    outlineColor: Color = MaterialTheme.colorScheme.surface
-) {
+fun EventProfilePics(uris: Iterable<String>, modifier: Modifier = Modifier) {
     Box(modifier) {
         uris.forEachIndexed { index, uri ->
+
+            val imageSize = 48.dp
+            val overlapPercentage = 0.5f
+
             AsyncImage(
                 model = uri,
                 placeholder = painterResource(R.drawable.ic_launcher_background),
                 error = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = null,
-                contentScale = ContentScale.Fit,
+                contentScale = ContentScale.FillWidth,
                 modifier = Modifier
                     .offset(x = if (index == 0) 0.dp else imageSize * (1 - overlapPercentage) * index)
-                    .border(outlineWidth, outlineColor, CircleShape)
+                    .border(2.dp, CardDefaults.cardColors().containerColor, CircleShape)
                     .clip(CircleShape)
                     .size(imageSize)
                     .align(Alignment.CenterStart)
