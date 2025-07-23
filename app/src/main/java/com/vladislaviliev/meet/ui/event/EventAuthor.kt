38ea841f4f8a3ce602632ecb4b5cbba43ccd931e
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-07-23 08:26:07 +0300

    Add contentScale to EventAuthor image

--- app/src/main/java/com/vladislaviliev/meet/ui/event/EventAuthor.kt
+++ app/src/main/java/com/vladislaviliev/meet/ui/event/EventAuthor.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +30,7 @@ internal fun EventAuthor(imgUri: String, name: String, occupation: String, modif
                 .clip(MaterialTheme.shapes.medium),
             placeholder = painterResource(id = R.drawable.ic_launcher_background),
             error = painterResource(id = R.drawable.ic_launcher_background),
+            contentScale = ContentScale.FillWidth,
         )
         Spacer(Modifier.width(12.dp))
         Column {
